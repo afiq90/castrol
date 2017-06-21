5f72d6526740dd4898e1f833331ab9d435a4dfa4
Afiq <mafiqh90@gmail.com>
2017-06-21 09:51:33 +0800

    Added SWReveal lib

---
+++ Castrol/HomeVC.swift
@@ -0,0 +1,41 @@
+//
+//  HomeVC.swift
+//  Castrol
+//
+//  Created by Afiq Hamdan on 6/20/17.
+//  Copyright © 2017 Cliqers. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rightViewRevealWidth = 150
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
